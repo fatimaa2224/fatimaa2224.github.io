28c821eed21c96c4d9b30360eb495724a630ce13
fatimaa2224 <55496582+fatimaa2224@users.noreply.github.com>
2019-10-07 09:42:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "fatima"
+let userName = "fatimaa2224";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
